5428af76f30af9fcb09e3aba943284bebb75bb10
Tokisaki Nino <trieuduytancbg@gmail.com>
2025-06-04 13:49:12 +0700

    mobile

---
+++ fe-mobile/lifetipsui/app/src/main/java/com/example/lifetipsui/apis/NotificationApi.kt
@@ -0,0 +1,6 @@
+package com.example.lifetipsui.apis
+import com.example.lifetipsui.config.Config
+
+object CategoryApi {
+    const val getListCategoryApi = "${Config.BASE_URL}/public/category/all"
+}
